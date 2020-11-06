8ed6e9343ce1b158ebcd69b101b378730e193b50
epsilon-aasim <aasim.khan3@epsilon.com>
2020-11-06 17:19:15 +0530

    Reset password

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from 'src/app/login/auth.service';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators } from "@angular/forms";
 import { SearchService } from 'src/app/search/search.service';
-
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +11,20 @@ import { SearchService } from 'src/app/search/search.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  passwordDialog: boolean;
+  passwordErrorMsg;
+  password: string;
+  confirmPassword: string;
+  passwordSubmitted: boolean;
   searchForm;
 
-  constructor(public authSerive: AuthService, public router: Router, public fb: FormBuilder, private searchService: SearchService) {
+  constructor(
+    public authSerive: AuthService,
+    public router: Router,
+    public fb: FormBuilder,
+    private messageService: MessageService,
+    private searchService: SearchService
+  ) {
     this.searchForm = this.fb.group({
       searchText: ['']
     })
@@ -34,5 +44,44 @@ export class HeaderComponent implements OnInit {
       }
     });
     this.searchService.changeMessage(this.searchForm.value.searchText)
+  }
+
+  /**
+   * @description Show Modal
+   */
+  passwordModal() {
+    this.passwordDialog = true;
+    this.clearModal();
+  }
+
+  /**
+   * @description Reset Password
+   */
+  resetPassword() {
+    this.passwordSubmitted = true;
+    let validConfPassword = this.confirmPassword && this.confirmPassword === this.password;
+    if (this.password && validConfPassword) {
+      console.log("Update Password >>>>>>>>>>>>>>")
+      this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Password updated', life: 3000 });
+      this.hideModal();
+    }
+  }
+
+  /**
+   * @description Hide Modal
+   */
+  hideModal() {
+    this.passwordDialog = false;
+    this.clearModal();
+  }
+
+  /**
+  * @description Clear Modal
+  */
+  clearModal() {
+    this.passwordErrorMsg = "";
+    this.password = "";
+    this.confirmPassword = "";
+    this.passwordSubmitted = false;
   }
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -7,14 +7,38 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from "@angular/forms";
 import { InterceptorService } from './interceptor.service';
-
+import { DialogModule } from 'primeng/dialog';
+import {ToastModule} from 'primeng/toast';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
-  declarations: [SidebarComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule, FontAwesomeModule, HttpClientModule, BsDropdownModule.forRoot(), ReactiveFormsModule, RouterModule],
-  exports: [SidebarComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    SidebarComponent, 
+    HeaderComponent, 
+    FooterComponent
+  ],
+  imports: [
+    CommonModule, 
+    FontAwesomeModule,
+    HttpClientModule, 
+    BsDropdownModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule, 
+    RouterModule,
+    DialogModule,
+    ToastModule,
+    PasswordModule,
+    ButtonModule
+  ],
+  exports: [
+    SidebarComponent, 
+    HeaderComponent, 
+    FooterComponent
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
