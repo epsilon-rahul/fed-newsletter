2d41092cc73f30e1d12253be58268d2610540e56
epsilon-rahul <rahbiyan@publicisgroupe.net>
2021-01-22 11:42:32 +0530

    Dashboard name rename to home and routing

--- frontend/src/app/login/login/login.component.ts
+++ frontend/src/app/login/login/login.component.ts
@@ -1,56 +1,57 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
-import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import { AuthService } from "../auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  loginForm = this.fb.group({
-    email: ["", Validators.required],
-    password: ["", [Validators.required]]
-  });
+    loginForm = this.fb.group({
+        email: ["", Validators.required],
+        password: ["", [Validators.required]],
+    });
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private myRoute: Router) {
-    if (this.authService.isLoggednIn()) {
-
-      this.myRoute.navigate(['dashboard']);
-
-    }
-  }
-
-  ngOnInit() { }
-
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    
-    if (this.loginForm.invalid) {
-      console.log('its coming here')
-      return;
-    } else {
-      let data = {
-        'email': this.loginForm.value.email,
-        'password': this.loginForm.value.password
-      }
-      console.log('its coming erer')
-      this.authService.login(data).subscribe(
-        response => {
-          this.authService.sendToken(response, response['status']);
-          let getCurrent = () => this.authService.getRole();
-          if ( getCurrent() === "admin" ) {
-            this.myRoute.navigate(['admin']);
-          }else{
-
-            this.myRoute.navigate(['dashboard']);
-          }
-        },
-        error => {
-          console.log(error);
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+        private myRoute: Router
+    ) {
+        if (this.authService.isLoggednIn()) {
+            this.myRoute.navigate(["home"]);
         }
-      )
     }
-  }
+
+    ngOnInit() {}
+
+    onSubmit() {
+        // TODO: Use EventEmitter with form value
+
+        if (this.loginForm.invalid) {
+            console.log("its coming here");
+            return;
+        } else {
+            let data = {
+                email: this.loginForm.value.email,
+                password: this.loginForm.value.password,
+            };
+            console.log("its coming erer");
+            this.authService.login(data).subscribe(
+                (response) => {
+                    this.authService.sendToken(response, response["status"]);
+                    let getCurrent = () => this.authService.getRole();
+                    if (getCurrent() === "admin") {
+                        this.myRoute.navigate(["admin"]);
+                    } else {
+                        this.myRoute.navigate(["home"]);
+                    }
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        }
+    }
 }
