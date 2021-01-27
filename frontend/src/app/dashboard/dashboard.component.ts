0f9df8ec3a6413353f0b5df7d127d58b99bd514f
epsilon-ankit <ankit.bhatia@epsilon.com>
2021-01-27 14:44:40 +0530

    merged conflicts

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { multi } from './data';
 import { EventServiceService } from "../event/event-service.service";
+import { ProjectService } from '../project/project.service';
 
 @Component({
     selector: "app-dashboard",
@@ -14,7 +15,8 @@ export class DashboardComponent implements OnInit {
     multi: any[];
     // view: any[] = [, 300];
     view: any[] = [1100, 400];
-
+    eventSlide1: any[];
+    eventSlide2: any[];
     // options
     legend: boolean = false;
     showLabels: boolean = true;
@@ -42,7 +44,7 @@ export class DashboardComponent implements OnInit {
     };
     cardColor: string = "#232837";
 
-    constructor(public projectService: ProjectService) {
+    constructor(public projectService: ProjectService, public eventService: EventServiceService) {
         Object.assign(this, { multi });
 
         this.first = [
@@ -146,114 +148,7 @@ export class DashboardComponent implements OnInit {
         ];
     }
 
-  single: any[];
-  two: any[];
-  multi: any[];
-  view: any[] = [, 300];
-  eventSlide1: any[];
-  eventSlide2: any[];
-
-  // options
-  legend: boolean = false;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = false;
-  showXAxisLabel: boolean = false;
-  xAxisLabel: string = 'Profit';
-  yAxisLabel: string = 'Project';
-  timeline: boolean = true;
-  first;
-
-  colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
-  };
-  cardColor: string = '#232837';
-
-
-
-  constructor(
-    public eventService: EventServiceService
-  ) {
-    Object.assign(this, { multi });
-
-
-
-    this.first = [
-      {
-        "name": "RFP requested",
-        "value": 8940000
-      },
-      {
-        "name": "RFP in-progress",
-        "value": 5000000
-      },
-      {
-        "name": "Proposal submitted",
-        "value": 7200000
-      },
-      {
-        "name": "Proposal under Review ",
-        "value": 5200000
-      },
-      {
-        "name": "SOW under Review",
-        "value": 7700000
-      },
-      {
-        "name": "SOW",
-        "value": 4300000
-      },
-      {
-        "name": "Verbal approval",
-        "value": 4300000
-      },
-      {
-        "name": "Won",
-        "value": 4300000
-      },
-      {
-        "name": "Business Development",
-        "value": 4300000
-      },
-      {
-        "name": "Proposal not shortlisted",
-        "value": 4300000
-      },
-      {
-        "name": "Closed",
-        "value": 4300000
-      }
-    ]
-
-    this.two = [
-      {
-        "name": "Financial",
-        "value": 8940000
-      },
-      {
-        "name": "Retail",
-        "value": 5000000
-      },
-      {
-        "name": "Media",
-        "value": 7200000
-      },
-      {
-        "name": "Healthcare",
-        "value": 5200000
-      },
-      {
-        "name": "Telecom",
-        "value": 7700000
-      },
-      {
-        "name": "Travel & Hospitality",
-        "value": 4300000
-      }
-    ]
-  }
+  
 
   ngOnInit() {
     this.single = this.first;
@@ -267,7 +162,7 @@ export class DashboardComponent implements OnInit {
     this.eventService.getEvents().subscribe(
         (resp) => {
           if(Array.isArray(resp)){
-            resp.reverse();
+            //resp.reverse();
             this.eventSlide1 = resp.slice(0, 3);
             this.eventSlide2 = resp.slice(3,6);
           }            
@@ -283,21 +178,13 @@ export class DashboardComponent implements OnInit {
   }
 
   change(e) {
-    console.log(e);
-    if (e == 'Vertical Domain') {
-
-    onSelect(event) {
-        console.log(event);
-    }
-
-    change(e) {
-        console.log(e);
-        if (e == "Vertical Domain") {
-            this.single = this.two;
-        } else if (e == "By Client") {
-            this.single = this.three;
-        } else {
-            this.single = this.first;
-        }
-    }
+      console.log(e);
+      if (e == "Vertical Domain") {
+          this.single = this.two;
+      } else if (e == "By Client") {
+          this.single = this.three;
+      } else {
+          this.single = this.first;
+      }
+  }
 }
